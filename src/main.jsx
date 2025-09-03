d3f6ec0e2cec05ed75c8a44c820242cfc085748c
Wanderley <davidgomeswanderley@gmail.com>
2025-09-03 17:42:15 -0300

    adicionando o outlet context para o router do site

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-//import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { Sobre } from "./pages/Sobre/Sobre";
 import { Contato } from "./pages/Contato/Contato";
 import { Portfolio } from "./pages/Portfolio/Portfolio";
 import { Orçamento } from "./pages/Orçamento/Orçamento";
 import { NotFound } from "./pages/NotFound/NotFound";
-
+import { MainLayout } from "./Layout/MainLayout"; // importa o layout
 import App from "./App";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/sobre",
-    element: <Sobre />,
-  },
-  {
-    path: "/portfolio",
-    element: <Portfolio />,
-  },
-  {
-    path: "/orçamento",
-    element: <Orçamento />,
-  },
-  {
-    path: "/contato",
-    element: <Contato />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <App /> },
+      { path: "sobre", element: <Sobre /> },
+      { path: "portfolio", element: <Portfolio /> },
+      { path: "orçamento", element: <Orçamento /> },
+      { path: "contato", element: <Contato /> },
+      { path: "*", element: <NotFound /> },
+    ],
   },
 ]);
 
